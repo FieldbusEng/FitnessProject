464939cc0adbbcb4a6066c7619b4b3f98b4c1983
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-08-23 16:55:33 +0200

    added Model Eating and Food

---
+++ ClassLibraryFitness/Model/Food.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryFitness.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        public double Proteins { get; set; }
+        /// <summary>
+        /// Fat food
+        /// </summary>
+        public double Fets { get; set; }
+        /// <summary>
+        /// Carbonate
+        /// </summary>
+        public double Carbohydrates { get; set; }
+        /// <summary>
+        /// Calories for 100gr
+        /// </summary>
+        public double Calories { get; }
+        
+        /// <summary>
+        /// empty constructor in case passed only name
+        /// </summary>
+        /// <param name="name"></param>
+        public Food(string name) : this(name,0,0,0,0)
+        {
+            
+        }
+
+        public Food(string name, double proteins, double fets, double carbohydrates, double calories)
+        {
+            Name = name;
+            Proteins = proteins /100.0;
+            Fets = fets / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+            Calories = calories / 100.0;
+            //todo: check
+
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+}
